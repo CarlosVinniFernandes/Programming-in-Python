81662526393fc38cf5bfe2b7f6cbdf553d7d2f21
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-01 20:33:46 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,6 @@
+numero1 = float(input('Selecione o primeiro número'))
+numero2 = float(input('Selecione o segundo número'))
+soma = numero1 + numero2
+m = numero1 * numero2
+print('A soma de', numero1, ('+'), numero2, ('é igual a'), soma, m)
+
