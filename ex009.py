8232aec5dce3749d9479a990b7e869ccf472ad8f
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-03 20:58:47 -0300

    Add files via upload

---
+++ ex009.py
@@ -0,0 +1,14 @@
+n = int(input('Escolha um número inteiro para ver sua tabuada'))
+a = n * 1
+b = n * 2
+c = n * 3
+d = n * 4
+e = n * 5
+f = n * 6
+g = n * 7
+h = n * 8
+i = n * 9
+j = n * 10
+print('-'*12)
+print('A tabuada do número {} é:\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(n, a, b, c, d, e, f, g, h, i, j))
+print('-'*12)
