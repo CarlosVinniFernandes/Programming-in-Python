d470c9e1c3eb35736b66a1896e5e5b0b8be0e8e1
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-02 22:55:20 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = float(input('Escolha um número'))
+d = n * 2
+t = n * 3
+r = n**(1/2)
+print('Seu número é o {}, \nseu dobro é {}, seu triplo é {} \ne sua raiz quadrara é {:.2f}'.format(n, d, t, r))
