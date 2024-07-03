c5775338e9f9e079b96415f9882eec54494ae035
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-03 20:59:16 -0300

    Add files via upload

---
+++ ex011.py
@@ -0,0 +1,5 @@
+l = float(input('Qual a largura da sua parede?'))
+a = float(input('Qual a altura da sua parede?'))
+area = l*a
+t = area/2
+print('A área da sua parede é igual a {}m² e necessita de {}L de tinta para pintar'.format(area, t))
