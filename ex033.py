f55713d6c6908a2aba124db25e9aa118d34b1b25
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-14 22:19:50 -0300

    Add files via upload

---
+++ ex033.py
@@ -0,0 +1,7 @@
+a = int(input('Escolha um número\n'))
+b = int(input('Escolha outro número\n'))
+c = int(input('Escolha mais um número\n'))
+if a > b and a > c:
+    print('A é o maior número')
+if a < b and a < c:
+    print('A é o menor número')
