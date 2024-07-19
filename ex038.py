3b44fd354ba3244ec4eaee35b3691669784ba3c6
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-18 22:43:21 -0300

    Add files via upload

---
+++ ex038.py
@@ -0,0 +1,8 @@
+num = int(input('Escreva um número:'))
+num2 = int(input('Escreva outro número:'))
+if num > num2:
+    print('O primeiro número é maior')
+elif num < num2:
+    print('O segundo número é maior')
+else:
+    print('Os números são iguais')
