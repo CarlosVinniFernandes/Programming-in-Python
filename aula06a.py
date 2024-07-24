0bb540f8a394966d862bbdf31612eff1ca91de21
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-23 23:15:36 -0300

    Add files via upload

---
+++ aula06a.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite um valor:'))
+n2 = float(input('Digite outro valor:'))
+s = n1 + n2
+#print('A soma de', n1, '+', ('o núumero'), n2, ('é igual a '), s)
+print('a soma entre {} e {} vale {}'.format(n1, n2, s))
