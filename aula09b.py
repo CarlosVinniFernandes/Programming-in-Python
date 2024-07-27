779634b125e2f84d7c25e1c6b45ccf353db9603a
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-07-26 22:01:41 -0300

    Add files via upload

---
+++ aula09b.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Vídeo Python'
+print(frase.replace('Curso', 'Aula'))
+print(frase.split())
+print('-'.join(frase.split()))
+print(frase.replace('Curso', 'Cesupa').upper())
+dividido = frase.split()
+print(dividido[2][3])
