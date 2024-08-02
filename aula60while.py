3251e8e6d114006159f009077ad727cdf740028c
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-01 23:08:10 -0300

    Add files via upload

---
+++ aula60while.py
@@ -0,0 +1,17 @@
+# # condicao = True
+# # while condicao:
+# #     nome = input('Qual seu name?')
+# #     condicao = False
+# #     break
+#       continue
+#
+# contador = 10
+# #
+# # while contador <10:
+# #     print('Eita')
+# #     contador += 1
+# #
+# # print('Acabou')
+# contador *= '2'
+# print(contador)
+
