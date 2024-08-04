1e0810df6c0b425369d1420ed04fca7f27120b48
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-03 21:36:26 -0300

    Add files via upload

---
+++ aula81.py
@@ -0,0 +1,13 @@
+# introducão ao desempacotamento + tuplas(tuplas)
+# nomes = ['Maria', 'Helena', 'Luiz']
+# nome1, nome2, nome3 = nomes
+# print(nome3)
+
+nomes = ['Maria', 'Helena', 'Luiz']
+nome1, *_ = nomes
+print(nome1, _)
+
+#tupla -> lista imutavel, funciona n usando colchetes
+
+
+
