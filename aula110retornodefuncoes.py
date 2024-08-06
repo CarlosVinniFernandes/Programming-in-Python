8beb928193e46e310f90b754fca2254138ac9c68
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-05 21:24:08 -0300

    Add files via upload

---
+++ aula110retornodefuncoes.py
@@ -0,0 +1,19 @@
+def soma(x,y):
+    return x+y
+
+soma1 = soma(2,2)
+soma2 = soma(3,3)
+print(soma1 + soma2)
+
+def soma2(x,y):
+    if x > 10:
+        return 10
+    return x+y
+
+soma1a = soma2(11,2)
+soma2a = soma2(3,3)
+print(soma1a)
+print(soma1a + soma2a)
+
+
+
