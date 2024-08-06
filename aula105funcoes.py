a2ffed4cb0024e2f34673c112ef4880a5ed00ed3
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-05 21:23:47 -0300

    Add files via upload

---
+++ aula105funcoes.py
@@ -0,0 +1,18 @@
+def Print():
+    print('Olá, mundo!')
+
+Print()
+
+def somar(a,b,c):
+    print(5+a)
+    print(5+b)
+    print(5+c)
+
+somar(5,7,9)
+
+def saudacao(nome='Sem nome'):
+    print(f'Olá, {nome}')
+
+saudacao('Carlos')
+saudacao()
+
