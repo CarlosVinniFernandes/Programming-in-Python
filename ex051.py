9015e9104c2629ad11013cee33e97a028166773f
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-09 22:01:48 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -1,5 +1,5 @@
 primeiro_termo = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão:  '))
+razao = int(input('Digite a razão: '))
 
 for i in range(10):
     termo_atual = primeiro_termo + i * razao
