278633c140c62cc1b6da5e49b0fa933bada01931
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-18 22:05:56 -0300

    Add files via upload

---
+++ ex7calculatorprofessor.py
@@ -0,0 +1,23 @@
+while True:
+    num1
+    num2
+    operador
+
+    numeros_validos = None
+
+    try:
+        numfloat
+        numfloat
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        um ou ambos numeros digitados sao invalidos
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido')
+        continue
