4dc15a30d91b221a8d077761c2b57ea7af9970a9
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2025-01-03 23:15:31 -0300

    Update something.py

--- something.py
+++ something.py
@@ -4,6 +4,13 @@ import re
 
 from pdfminer.high_level import extract_pages, extract_text
 
-text = extract_text("xxxxxx")
-texto_filtrado = re.sub(r'[^\w\s]', '', text)  
-print(texto_filtrado)
+text = extract_text("xxxxxxxxxx")
+texto_filtrado = re.sub(r'[^\w\s./;,\\]', '', text)
+
+ocorrencias = [m.start() for m in re.finditer('XXXXXXX', texto_filtrado)]
+
+for i in range(len(ocorrencias) - 1):
+    inicio = ocorrencias[i] + len('XXXXXXX')
+    fim = ocorrencias[i + 1]
+    texto_XXXXXXX = texto_filtrado[inicio:fim].strip()
+    print(f"XXXXXXX {i + 1}:\n{texto_XXXXXXX}\n")
